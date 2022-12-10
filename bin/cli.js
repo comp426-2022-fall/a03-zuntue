2c364e230fa16af42e152357238dd6ea7948dab3
zuntue <zuntue@gmail.com>
2022-12-10 00:47:21 -0500

    tests are running smoothly

--- bin/cli.js
+++ bin/cli.js
@@ -4,10 +4,9 @@ import roll from '../lib/roll.js'
 
 const args = minimist(process.argv.slice(2))
 
-
-const sides = args.sides > 0 ? args.sides: 6;
-const dice = args.dice > 0 ? args.dice: 2;
-const rolls = args.rolls > 0 ? args.rolls: 1;
+const sides = args.sides > 0 ? args.sides : 6;
+const dice = args.dice > 0 ? args.dice : 2;
+const rolls = args.rolls > 0 ? args.rolls : 1;
 
 const output = roll(sides, dice, rolls);
 
